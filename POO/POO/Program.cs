7e333d83c71dd107c7de2b0e086be3714bcd2d09
KeyzDev <86132907+Marigiko@users.noreply.github.com>
2022-09-15 22:55:42 -0300

    POO

---
+++ POO/POO/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using POO;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+int cantidadTransportes;
+int i = 1;
+
+cantidadTransportes = 1;
+List<TransportePublico> listaDeTrasportes = new List<TransportePublico>();
+Console.WriteLine("El taxi tiene una capacidad maxima de 4 pasajeros\n y el omnibus tiene una capacidad maxima de 100 pasajeros");
+
+while (i <= cantidadTransportes)
+{
+    Console.WriteLine("Ingrese cantidad de pasajeros para el taxi: ");
+    Taxi taxi = new Taxi();
+    taxi.setPasajeros(Convert.ToInt16(Console.ReadLine()));
+
+    Console.WriteLine("Ingrese cantidad de pasajeros para el omnibus: ");
+    Omnibus omnibus = new Omnibus();
+    omnibus.setPasajeros(Convert.ToInt16(Console.ReadLine()));
+
+    listaDeTrasportes.Add(taxi);
+    listaDeTrasportes.Add(omnibus);
+
+    i++;
+}
+
+foreach (TransportePublico tp in listaDeTrasportes)
+{
+    Console.WriteLine($"{tp.ToString().Substring(4)}: {tp.getPasajeros()} pasajeros");
+}
---
+++ POO/POO/TransportePublico.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    abstract class TransportePublico
+    {
+        private int pasajeros;
+        protected int capacidadMaxima;
+
+        public int getPasajeros()
+        {
+            return this.pasajeros;
+        }
+        public void setPasajeros(int pasajeros)
+        {
+            if(pasajeros <= this.capacidadMaxima)
+            {
+                this.pasajeros = pasajeros;
+            }
+        }
+        public int getCapacidadMaxima()
+        {
+            return this.capacidadMaxima;
+        }
+
+        public void Avanzar(){}
+        public void Detenerse(){}
+    }
+}
